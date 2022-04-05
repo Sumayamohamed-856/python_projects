a0a632fdb0c12b9f1f48c8ca9d4a108b14874fd4
Sumayamohamed <Fahimaqalanjo818@gmail.com>
2022-04-05 11:24:35 -0700

    database

---
+++ index.py
@@ -0,0 +1,456 @@
+# numbers in python
+
+product_id = 123 
+sale_price = 14.99
+tip_percentage = 1/5
+new_product = 150
+
+print(sale_price + new_product)
+
+# integer
+# float
+# fraction
+# integer
+
+# order of opearations in python
+
+print('Addition')
+print(100 + 42)
+
+print('Subtraction')
+print(100 - 42)
+
+print('Division')
+print(100 / 42)
+print(100 / 38)
+
+print('Floor Division')
+print(100 // 42)
+print(100 // 38)
+
+print('Multiplication')
+print(100 * 42)
+
+print('Modulus')
+print(100 % 42)
+
+print('Exponents')
+print(100 ** 42)
+
+# assignment operators in python
+
+total = 100
+
+total = total + 10
+total += 10
+total -= 10
+total *= 2
+total /= 10
+total //= 10
+total **= 2
+total %= 2
+
+product_two = 120
+product_three = 10
+
+total += product_two
+total += product_three
+
+print(total)
+
+# decimal vs float in python
+
+from decimal import Decimal
+
+product_cost = 88.40
+commission_rate = 0.08
+qty = 450
+
+product_cost += (commission_rate * product_cost)
+print(product_cost * qty) # 42962.4
+
+product_cost = Decimal(88.40)
+commission_rate = Decimal(0.08)
+qty = 450
+
+product_cost += (commission_rate * product_cost)
+print(product_cost * qty) # 42962.40000000000282883716451
+
+
+# integer, complex, float, decimal
+
+from decimal import Decimal
+
+product_cost = 88.80
+commission_rate = 0.08
+qty = 450
+
+print(int(product_cost))
+print(float(qty))
+print(Decimal(product_cost))
+print(complex(commission_rate))
+
+# popular math in python
+
+import math
+
+loss = -20.25
+product_cost = 89.99
+
+print(abs(loss))
+print(math.floor(product_cost))
+print(math.ceil(product_cost))
+print(abs(math.floor(loss)))
+print(round(product_cost))
+print(math.sqrt(product_cost))
+print(math.pow(5, 2))
+print(5 ** 2)
+
+# order of operation in python
+
+"""
+Please -> Parans ()
+Excuse -> Exponents **
+My -> Multiplication *
+Dear -> Division /
+Aunt -> Addition +
+Sally -> Subtraction -
+
+8 + 2 * 5 - (9 + 2) ** 2
+8 + 2 * 5 - 11 ** 2
+8 + 2 * 5 - 121
+8 + 10 - 121
+-103
+"""
+
+calculation = 8 + 2 * 5 - (9 + 2) ** 2
+
+print(calculation)
+
+# manual exponent
+
+def manual_exponent(num, exp):
+  counter = exp - 1
+  total = num
+
+  while counter > 0:
+    total *= num
+    counter -= 1
+
+  return total
+
+print(manual_exponent(2, 3))
+print(manual_exponent(10, 2))
+
+# reduce function
+
+from functools import reduce
+
+def manual_exponent(num, exp):
+  computed_list = [num] * exp
+  return(reduce(lambda total, element: total * element, computed_list))
+
+print(manual_exponent(2, 3))
+print(manual_exponent(10, 2))
+
+# overview lists in python
+
+"""
+User Database Query
+Kristine
+Tiffany
+Jordan
+"""
+
+users = ['Kristine', 'Tiffany', 'Jordan']
+
+print(users)
+
+users.insert(1, 'Anthony')
+
+print(users)
+
+users.append('Ian')
+
+print(users)
+
+print([users[2]])
+
+users[4] = 'Brayden'
+
+print(users)
+
+# removing element lists in python
+
+users = ['Kristine', 'Tiffany', 'Jordan', 'Leann']
+
+print(users)
+
+users.remove('Jordan')
+
+print(users)
+
+popped_user = users.pop()
+
+print(popped_user)
+print(users)
+
+del users[0]
+
+print(users)
+
+# nested lists
+
+users = ['Kristine', 'Tiffany', 'Jordan', 'Leann']
+
+ids = [1, 2, 3, 4]
+
+mixed_list = [42, 10.3, 'Altuve', users]
+
+print(mixed_list)
+
+user_list = mixed_list.pop()
+
+print(user_list)
+print(mixed_list)
+
+nested_lists = [[123], [234], [345]]
+
+# negative indexes in python
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+number_of_tags = len(tags)
+last_item = tags[-1]
+index_of_last_item = tags.index(last_item)
+
+print(number_of_tags)
+print(last_item)
+print(index_of_last_item)
+
+# sort lists in python
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+print(tags)
+
+tags.sort()
+
+print(tags)
+
+tags.sort(reverse=True)
+
+print(tags)
+
+totals = [234, 1, 543, 2345]
+
+totals.sort(reverse=True)
+
+print(totals)
+
+# url query string in python
+
+# https://www.google.com/search?q=python+development+tutorial
+
+uri = 'https://www.google.com/search?q='
+tags = ['python', 'development', 'tutorial']
+formatted_tags = '+'.join(tags)
+query_uri = f'{uri}{formatted_tags}'
+
+print(query_uri)
+
+# ranges in python lists
+
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+tag_range = tags[2:]
+tag_range = tags[0:2]
+tag_range = tags[:2]
+tag_range = tags[0:-1]
+
+print(tag_range)
+
+# slices and ranges
+
+tags = [
+  'python',
+  'development',
+  'tutorials',
+  'code',
+  'programming',
+  'computer science'
+]
+
+tag_range = tags[1:-1:2]
+tag_range = tags[::-1]
+
+print(tag_range)
+
+tags.sort(reverse=True)
+
+print(tags)
+
+# sorted function
+
+sale_prices = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+
+sorted_list = sorted(sale_prices, reverse=True)
+
+print(sorted_list)
+
+# how to find the median
+
+# Tools:
+# - math library
+# - sorted function
+# - list slicing
+# - computations
+
+sale_prices = [
+  100,
+  83,
+  220,
+  40,
+  100,
+  400,
+  10,
+  1,
+  3
+]
+
+sorted_list = sorted(sale_prices)
+num_of_sales = len(sorted_list)
+half_slice = math.floor(num_of_sales/2)
+first_sales_items = sorted_list[:half_slice]
+last_sales_items = sorted_list[-(half_slice):]
+median = sorted_list[half_slice:(half_slice + 1)]
+
+print(sorted_list)
+print(num_of_sales)
+print(first_sales_items)
+print(last_sales_items)
+print(median)
+
+# slice class
+
+tags = [
+  'python',
+  'development',
+  'tutorials',
+  'code',
+  'programming',
+]
+
+print(tags[1:4:2])
+
+slice_obj = slice(1, 4, 2)
+
+print(slice_obj.start)
+print(slice_obj.stop)
+print(slice_obj.step)
+
+print(tags[slice_obj])
+
+# Add to a list with in place and copy processes
+
+tags = ['python', 'development', 'tutorials', 'code']
+
+# Nope
+tags[-1] = 'Programming'
+
+# In Place
+tags.extend('Programming')
+tags.extend(['Programming'])
+
+# New List
+new_tags = tags + ['Programming']
+
+print(new_tags)
+
+print(tags)
+
+# weighted lottery
+
+# import numpy as np
+
+def weighted_lottery(weights):
+    container_list = []
+
+    for (name, weight) in weights.items():
+        for _ in range(weight):
+            container_list.append(name)
+
+    # return np.random.choice(container_list)
+
+#  weights = {
+#      'winning': 1,
+#      'losing': 1000
+#  }
+#
+#  print(weighted_lottery(weights))
+
+other_weights = {
+    'green': 1,
+    'yellow': 2,
+    'red': 3
+}
+
+print(weighted_lottery(other_weights))
+
+
+
+# overview of python dictionaries
+
+players = {
+  "ss": "Correa",
+  "2b": "Altuve",
+  "3b": "Bregman",
+  "DH": "Gattis",
+  "OF": "Springer",
+}
+
+second_base = players['2b']
+designated_hitter = players['DH']
+
+print(designated_hitter)
+
+
+# nested collections in python dictionaries
+
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"],
+}
+
+astros = teams['astros']
+print(astros)
+print(teams['angels'])
+print(teams['yankees'])
+
+# add new key/value python pairs in dictionaries
+
+teams = {
+  "astros": ["Altuve", "Correa", "Bregman"],
+  "angels": ["Trout", "Pujols"],
+  "yankees": ["Judge", "Stanton"]
+}
+
+teams['red sox'] = ['Price', 'Betts']
+
+print(teams)
+
+
+# get functions
+
--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def manual_incrementing_matrix(n):
 
   counter = 0
 
-  for idx, el in enumerate(matrix):
+  for idx, el in enumerate(matrix): 
     for nested_idx, nested_el in enumerate(el):
       matrix[idx][nested_idx] = counter + nested_idx
     counter += 1
@@ -79,3 +79,258 @@ sentence_three = "That is my dog's bowl"
 sentence_four = "Tiffany said, \"That is my dog's bowl\""
 
 print(sentence_four)
+
+
+# upper, lower case, capitalize on strings
+
+
+sentence = 'The quick brown fox jumped'
+sentence_two = sentence.upper()
+
+print(sentence)
+print(sentence_two)
+
+sentence = 'the quick brown fox jumped'.capitalize()
+print(sentence)
+
+sentence = 'the quick brown fox jumped'.title()
+print(sentence)
+
+sentence = 'the Quick Brown fOx jumped'
+print(sentence.lower())
+
+
+# number rogers
+
+# The quick brown fox jumped
+# T => 0
+# h => 1
+# e => 2
+# ' ' => 3
+
+starter_sentence = 'The quick brown fox jumped'
+
+# first = starter_sentence[0]
+
+# second = starter_sentence[1]
+
+# third = starter_sentence[2]
+
+# new_sentence = first + second + third
+
+# print(new_sentence)
+
+first_word = starter_sentence[0:3]
+new_sentence = 'thy' + starter_sentence[3:]
+print(new_sentence)
+
+
+# Heredoc
+
+content = """
+Nullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.
+
+Vestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.
+
+Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+""".strip()
+
+print(content)
+
+
+# line breakes, multi string sentences
+
+# Integer posuere erat a ante venenatis dapibus posuere velit aliquet.
+# """
+
+# print(repr(content))
+
+long_string = '\nNullam id dolor id nibh ultricies vehicula ut id elit. Nullam quis risus eget urna mollis ornare vel eu leo.\n\nVestibulum id ligula porta felis euismod semper. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Cras justo odio, dapibus ac facilisis in.\n\nInteger posuere erat a ante venenatis dapibus posuere velit aliquet.\n'
+
+print(long_string)
+
+
+# functions in python
+
+import operator
+from functools import reduce
+
+def dynamic_reducer(collection, op):
+    operators = {
+        "+": operator.add,
+        "_": operator.sub,
+        "*": operator.mul,
+        "/": operator.truediv
+}
+    return reduce((lambda total, element: 
+    operators[op](total, element)), collection)
+
+print(dynamic_reducer([1, 2, 3], '+'))
+print(dynamic_reducer([1, 2, 3], '_'))
+print(dynamic_reducer([1, 2, 3], '*'))
+print(dynamic_reducer([1, 2, 3], '/'))
+
+
+# string interpolation
+
+name = 'Kristine'
+customer = f'Hi {name}'
+greeting = f'This is my {{bracket}} blog test'
+print(greeting)
+print(customer)
+
+name = 'Kristine'
+customer = f'Hi {name}'
+greeting = f'This is my {{bracket}} blog test'
+product = 'Python elearning course'
+
+email_content = f"""
+Hi {name}
+
+Thank you for purchasing {product}
+
+Regards,
+
+Sales Team
+"""
+print(greeting)
+print(customer)
+print(email_content)
+
+# old format
+
+name = 'Kristine'
+age = 12
+product = 'Python eLearning course'
+from_account = 'Jordan'
+
+greeting = "Product Purchase: {2} - Hi {0}, you are listed as {1} years old. - {3}".format(name, age, product, 'Jordan')
+
+greeting = f"Product Purchase: {product} - Hi {name}, you are listed as {age} years old. - {from_account}"
+
+print(greeting)
+
+
+# query string methods
+
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+# query = sentence.find('oops')
+# query_two = sentence.index('oops')
+
+# print(query)
+# print(query_two)
+
+query = 'oops' in sentence
+
+print(query)
+
+# replace function in string
+
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+sentence = sentence.replace('quick', 'slow')
+
+print(sentence.replace('quick', 'slow'))
+print(sentence)
+
+# working with negative index
+
+sentence = 'The quick brown fox jumped over the lazy dog.'
+
+print(sentence[-4:])
+
+# python strip
+
+url = 'https://google.com'
+
+print(url.strip())
+
+print(url.strip('https://'))
+
+url = url.lstrip('https://')
+url = url.rstrip('.com')
+
+print(url.capitalize())
+
+# partition
+
+heading = "Python: An Introduction, and Python: Advanced"
+
+header, _, subheader = heading.partition(': ')
+
+print(header)
+print(subheader)
+
+first, second, third = heading.partition(': ')
+
+print(first)
+print(second)
+print(third)
+
+# splits in string
+
+tags = 'python,coding,programming,development'
+
+list_of_tags = tags.split(',')
+list_of_tags = tags.split()
+
+print(list_of_tags)
+
+# heading = "Python: An Introduction"
+
+# heading, subheading = heading.split(': ')
+
+# print(heading)
+# print(subheading)
+
+# Alpha numric and numric functions in strings
+
+api_data = '5'
+greeting = 'Hi there'
+
+print(api_data.isalpha())
+print(greeting.isalpha())
+
+print(api_data.isnumeric())
+print(greeting.isnumeric())
+
+# checkpoint 3
+
+greeting = 'Welcome back'
+numbers = 5
+meeting = True
+list_items = ['books', 'pens', 'laptops']
+names = 'harry,alex,susie,jared,gail,conner'
+
+index = greeting[0:3]
+string = greeting[0:7]
+string_two = greeting.upper()
+
+items_lists = list_items
+
+first_element = list_items[-3]
+
+name_lists = names.split(',')
+
+addition_of_numbers = numbers + 5
+
+last_element = list_items[-1]
+
+age = f'Under below age {numbers} you qualify for free purchase'
+
+customers = "hello world."
+
+print(index)
+print(items_lists)
+print(first_element)
+print(addition_of_numbers)
+print(last_element)
+print(name_lists)
+print(string)
+print(string_two)
+print(age)
+print(customers)
+
+# A descriptive logo is that textual and visual design elements and 
+# indistinctive logo is distinctive logo are more like image brand and they show off personality brand
